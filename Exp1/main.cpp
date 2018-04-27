a219a836b3f7c1de09da1afbe953f5bb23bf0992
AlanXue1997 <836106158@qq.com>
2018-04-27 22:58:31 +0800

    common commit

--- Exp1/main.cpp
+++ Exp1/main.cpp
@@ -11,7 +11,7 @@
 //Http 重要头部数据
 struct HttpHeader {
 	char method[4]; // POST 或者 GET，注意有些为 CONNECT，本实验暂不考虑
-		char url[1024]; // 请求的 url
+	char url[1024]; // 请求的 url
 	char host[1024]; // 目标主机
 	char cookie[1024 * 10]; //cookie
 	HttpHeader() {
@@ -19,14 +19,30 @@ struct HttpHeader {
 	}
 };
 
+struct ADV_P {
+	struct ProxyParam* lpProxyParam;
+	char* IP_addr;
+};
+
 BOOL InitSocket();
-void ParseHttpHead(char *buffer, HttpHeader * httpHeader);
+void ParseHttpHead(char *buffer, HttpHeader * httpHeader, char *IP_addr);
 BOOL ConnectToServer(SOCKET *serverSocket, char *host);
-unsigned int __stdcall ProxyThread(LPVOID lpParameter);
+unsigned int __stdcall ProxyThread(LPVOID adv_p);
 //代理相关参数
 SOCKET ProxyServer;
 sockaddr_in ProxyServerAddr;
 const int ProxyPort = 10240;
+
+char* WhiteIP[] = {
+	"172.20.12.134",
+	NULL
+};
+
+char* LimitedAddr[] = {
+	"http://jwes.hit.edu.cn/",
+	NULL
+};
+
 //由于新的连接都使用新线程进行处理，对线程的频繁的创建和销毁特别浪费资源
 //可以使用线程池技术提高服务器效率
 //const int ProxyThreadMaxNum = 20;
@@ -52,14 +68,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	DWORD dwThreadID;
 	//代理服务器不断监听
 	while (true) {
-		acceptSocket = accept(ProxyServer, NULL, NULL);
+		sockaddr_in addr;
+		int len = sizeof(addr);
+		acceptSocket = accept(ProxyServer, (sockaddr*)&addr, &len);
+		char *ip_addr = new char[16];
+		strcpy(ip_addr, inet_ntoa(addr.sin_addr));
+		printf("--->>> %s\n", inet_ntoa(addr.sin_addr));
 		lpProxyParam = new ProxyParam;
 		if (lpProxyParam == NULL) {
 			continue;
 		}
 		lpProxyParam->clientSocket = acceptSocket;
-		hThread = (HANDLE)_beginthreadex(NULL, 0,
-			&ProxyThread, (LPVOID)lpProxyParam, 0, 0);
+		hThread = (HANDLE)_beginthreadex(NULL, 0, &ProxyThread, (LPVOID)(new ADV_P{ lpProxyParam, ip_addr}), 0, 0);
 		CloseHandle(hThread);
 		Sleep(200);
 	}
@@ -123,7 +143,9 @@ BOOL InitSocket() {
 // Qualifier: 线程执行函数
 // Parameter: LPVOID lpParameter
 //************************************
-unsigned int __stdcall ProxyThread(LPVOID lpParameter) {
+unsigned int __stdcall ProxyThread(LPVOID adv_p) {
+	char * IP_addr = ((ADV_P*)adv_p)->IP_addr;
+	ProxyParam *lpParameter = ((ADV_P*)adv_p)->lpProxyParam;
 	char Buffer[MAXSIZE];
 	char *CacheBuffer;
 	ZeroMemory(Buffer, MAXSIZE);
@@ -131,8 +153,7 @@ unsigned int __stdcall ProxyThread(LPVOID lpParameter) {
 	int length = sizeof(SOCKADDR_IN);
 	int recvSize;
 	int ret;
-	recvSize = recv(((ProxyParam
-		*)lpParameter)->clientSocket, Buffer, MAXSIZE, 0);
+	recvSize = recv(((ProxyParam*)lpParameter)->clientSocket, Buffer, MAXSIZE, 0);
 	if (recvSize <= 0) {
 		goto error;
 	}
@@ -140,25 +161,21 @@ unsigned int __stdcall ProxyThread(LPVOID lpParameter) {
 	CacheBuffer = new char[recvSize + 1];
 	ZeroMemory(CacheBuffer, recvSize + 1);
 	memcpy(CacheBuffer, Buffer, recvSize);
-	ParseHttpHead(CacheBuffer, httpHeader);
+	ParseHttpHead(CacheBuffer, httpHeader, IP_addr);
 	delete CacheBuffer;
-	if (!ConnectToServer(&((ProxyParam
-		*)lpParameter)->serverSocket, httpHeader->host)) {
+	if (!ConnectToServer(&((ProxyParam*)lpParameter)->serverSocket, httpHeader->host)) {
 		goto error;
 	}
 	printf("代理连接主机 %s 成功\n", httpHeader->host);
 	//将客户端发送的 HTTP 数据报文直接转发给目标服务器
-	ret = send(((ProxyParam *)lpParameter)->serverSocket, Buffer, strlen(Buffer)
-		+ 1, 0);
+	ret = send(((ProxyParam *)lpParameter)->serverSocket, Buffer, strlen(Buffer) + 1, 0);
 	//等待目标服务器返回数据
-	recvSize = recv(((ProxyParam
-		*)lpParameter)->serverSocket, Buffer, MAXSIZE, 0);
+	recvSize = recv(((ProxyParam*)lpParameter)->serverSocket, Buffer, MAXSIZE, 0);
 	if (recvSize <= 0) {
 		goto error;
 	}
 	//将目标服务器返回的数据直接转发给客户端
-	ret = send(((ProxyParam
-		*)lpParameter)->clientSocket, Buffer, sizeof(Buffer), 0);
+	ret = send(((ProxyParam*)lpParameter)->clientSocket, Buffer, sizeof(Buffer), 0);
 	//错误处理
 error:
 	printf("关闭套接字\n");
@@ -179,13 +196,14 @@ error:
 // Parameter: char * buffer
 // Parameter: HttpHeader * httpHeader
 //************************************
-void ParseHttpHead(char *buffer, HttpHeader * httpHeader) {
+void ParseHttpHead(char *buffer, HttpHeader * httpHeader, char *IP_addr) {
 	char *p;
 	char *ptr;
 	const char * delim = "\r\n";
 	p = strtok_s(buffer, delim, &ptr);//提取第一行
 	printf("%s\n", p);
 	if (p[0] == 'G') {//GET 方式
+		//p = "GET today.hit.edu.cn HTTP/1.1";
 		memcpy(httpHeader->method, "GET", 3);
 		memcpy(httpHeader->url, &p[4], strlen(p) - 13);
 	}
@@ -194,10 +212,30 @@ void ParseHttpHead(char *buffer, HttpHeader * httpHeader) {
 		memcpy(httpHeader->url, &p[5], strlen(p) - 14);
 	}
 	printf("%s\n", httpHeader->url);
+	int flag = 0;
+	int limited = 0;
+	while (LimitedAddr[flag] != NULL) {
+		if (strcmp(LimitedAddr[flag], httpHeader->url) == 0) {
+			limited = 1;
+			break;
+		}
+		flag++;
+	}
+	if (limited) {
+		flag = 0;
+		while (WhiteIP[flag] != NULL) {
+			if (strcmp(WhiteIP[flag], IP_addr) == 0) {
+				return;
+			}
+			flag++;
+		}
+	}
 	p = strtok_s(NULL, delim, &ptr);
 	while (p) {
 		switch (p[0]) {
 		case 'H'://Host
+			printf("--->>> host : %s", p);
+			//p="Host: today.hit.edu.cn";
 			memcpy(httpHeader->host, &p[6], strlen(p) - 6);
 			break;
 		case 'C'://Cookie
